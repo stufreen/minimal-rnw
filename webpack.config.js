4323b044324bdc3cc0602d849b36c8120d8d3142
Stu Freen <stu.freen@shopify.com>
2020-01-23 13:14:09 -0500

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
